9a54b7db76975d24b73af5e9db7c424acdd6c725
jeisacarvalho <jeisasilva1823@gmail.com>
2023-06-03 21:56:22 -0300

    react

---
+++ modulo-02/arrayType/arrayType.js
@@ -0,0 +1,4 @@
+"use strict";
+// => Exemplo 01 - Uso de Colchetes:
+let frutas = ['abacaxi', 'uva', 'maça', 'melancia', 'morango'];
+console.log(frutas[2]);
---
+++ modulo-02/arrayType/arrayType.ts
@@ -0,0 +1,6 @@
+// => Exemplo 01 - Uso de Colchetes:
+
+let frutas: string[] = ['abacaxi', 'uva', 'maça','melancia','morango']
+console.log(frutas[2])
+
+// => Exemplo 02 - Array Object (Objeto Array)
